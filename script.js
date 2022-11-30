e280f6a694e70683c771e5f2b882a3cd5aa78901
jmilljr24 <jmilljr24@gmail.com>
2022-11-30 12:55:39 -0500

    Add comments

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const container = document.querySelector("#container");
+//Default grid size
 let gridSize = 256;
 let gridRows = 16;
 
+const container = document.querySelector("#container");
+
+//create total number of grid elements
 function createGrid() {
   for (let i = 1; i <= gridSize; i++) {
-    const verticalBox = document.createElement("div");
-    verticalBox.classList.add("box");
-    verticalBox.id = i;
-    verticalBox.textContent = "";
-    container.appendChild(verticalBox);
+    const divBox = document.createElement("div");
+    divBox.classList.add("box");
+    divBox.id = i;
+    divBox.textContent = "";
+    container.appendChild(divBox);
   }
 }
 createGrid();
@@ -17,11 +20,16 @@ let root = document.querySelector(":root");
 
 function setDim() {
   gridRows = document.getElementById("userInput").value;
-  gridRows = Number(gridRows);
-  gridSize = Math.pow(gridRows, 2);
-  root.style.setProperty("--grid-rows", gridRows);
-  removeAllChildNodes(container);
-  createGrid();
+  if (gridRows > 100) {
+    alert("Max dimensions of 100 x 100! Enter a smaller number.");
+    return;
+  } else {
+    gridRows = Number(gridRows);
+    gridSize = Math.pow(gridRows, 2); //length of grid squared for total number of elements
+    root.style.setProperty("--grid-rows", gridRows); //set css variable to number of rows for a square
+    removeAllChildNodes(container);
+    createGrid();
+  }
 }
 
 function removeAllChildNodes(parent) {
@@ -30,6 +38,7 @@ function removeAllChildNodes(parent) {
   }
 }
 
+//mouse over changes background color from default white to black
 const hover = document.getElementById("container");
 hover.addEventListener("mouseover", (event) => {
   event.target.style.backgroundColor = "black";
