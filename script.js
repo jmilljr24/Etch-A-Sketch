beb80d692210f72ab856aecad7b3e6563f69a095
jmilljr24 <jmilljr24@gmail.com>
2022-11-30 12:19:12 -0500

    Add grid of elements with text input for dimensions
    user input with set grid rows with a function to create the the total elements from the square of the number of rows.

---
+++ script.js
@@ -0,0 +1,31 @@
+const container = document.querySelector("#container");
+let gridSize = 256;
+let gridRows = 16;
+
+function createGrid() {
+  for (let i = 1; i <= gridSize; i++) {
+    const verticalBox = document.createElement("div");
+    verticalBox.classList.add("box");
+    verticalBox.id = i;
+    verticalBox.textContent = "";
+    container.appendChild(verticalBox);
+  }
+}
+createGrid();
+
+let root = document.querySelector(":root");
+
+function setDim() {
+  gridRows = document.getElementById("userInput").value;
+  gridRows = Number(gridRows);
+  gridSize = Math.pow(gridRows, 2);
+  root.style.setProperty("--grid-rows", gridRows);
+  removeAllChildNodes(container);
+  createGrid();
+}
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild);
+  }
+}
